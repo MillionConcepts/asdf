4cbd755a9cc5256009c92932546aa1efcfc4feb7
mstclair@millionconcepts.com <mstclair@millionconcepts.com>
2021-09-16 16:27:12 -0400

    allow caltargets in rapid notebook

--- rapid/helpers.py
+++ rapid/helpers.py
@@ -7,19 +7,23 @@ from asdf.scan import find_obs_pixmaps, cluster_observations, scan_zcam_files
 from asdf.zcam_bandset import ZcamBandSet
 
 warnings.filterwarnings("ignore", category=NotGeoreferencedWarning)
-np.seterr(divide='ignore', invalid='ignore')
+np.seterr(divide="ignore", invalid="ignore")
 
 
-def get_zcam_bandset(image_path, roi_path=None, use_pixmaps=True):
+def get_zcam_bandset(
+    image_path, roi_path=None, use_pixmaps=True, keep_caltarget=False
+):
     observations = scan_zcam_files(image_path)
-    observation = list(cluster_observations(observations)[0].values())[0]
+    clusters = cluster_observations(observations, keep_caltarget=keep_caltarget)
+    observation = list(clusters[0].values())[0]
     zband = ZcamBandSet(observation)
     if use_pixmaps is True:
-        pixes = find_obs_pixmaps(zband.metadata['PATH'])[0]
-        zband.associate_pixmaps(pixes)
-        zband.load_pixmaps()
-    zband.load('all')
-    zband.bulk_debayer('all')
+        pixes = find_obs_pixmaps(zband.metadata["PATH"])[0]
+        if pixes:
+            zband.associate_pixmaps(pixes)
+            zband.load_pixmaps()
+    zband.load("all")
+    zband.bulk_debayer("all")
     if roi_path is not None:
         zband.rois = roi_path
         zband.load_rois()
@@ -28,5 +32,3 @@ def get_zcam_bandset(image_path, roi_path=None, use_pixmaps=True):
         zband.count_pixmaps()
     zband.format_metadata()
     return zband
-
-
