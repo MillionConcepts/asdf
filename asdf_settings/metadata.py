9d3b1f4aaa2c582f31b226b0d78979091c7ebae7
Sierra-MC <sierra@millionconcepts.com>
2022-09-05 16:00:40 -0400

    In-production fully versioned

--- asdf_settings/metadata.py
+++ asdf_settings/metadata.py
@@ -6,6 +6,8 @@ these literals should generally be safe; removing them may not be.
 # don't change this
 from itertools import chain
 
+from .metagenerators import FILTER_DATA_COLUMNS
+
 # lookup table for location by sol -- number is final sol of location
 LOCATION_TABLE = {
     101: "Octavia E. Butler Landing",
@@ -117,63 +119,59 @@ RC_METADATA_COLUMNS = (
     "AZIMUTH_ANGLE",
     "EMISSION_ANGLE",
     "SCALING_FACTOR",
-    "UNCERTAINTY",
-    "SEL_FILE"
+    "UNCERTAINTY"
 )
 
-# Only the metadata fields here will appear in the compact -marslab.csv file.
+# Only the columns listed here will appear in the compact -marslab.csv file.
+# Their order here is preserved in the .csv file.
 # *ROI_METADATA_FIELDS are the user-input fields defined above.
+# *FILTER_DATA_COLUMNS are the per-filter mean/std pixel count columns.
 COMPACT_ZCAM_MARSLAB_FIELDS = (
     "NAME",
     "COLOR",
     "ANALYSIS_NAME",
     "SOL",
     "SEQ_ID",
-    "FEATURE",
-    "DESCRIPTION",
-    "SITE",
-    "DRIVE",
-    "RSM",
-    "LTST",
-    "INCIDENCE_ANGLE",
-    "EMISSION_ANGLE",
-    "PHASE_ANGLE",
-    "SOLAR_ELEVATION",
-    "SOLAR_AZIMUTH",
-    "LAT",
-    "LON",
-    "ODOMETRY",
-    "ROVER_ELEVATION",
-    "TARGET_ELEVATION",
-    "INSTRUMENT",
-    "SCLK",
     *ROI_METADATA_FIELDS,
     # TODO: determine if this moves somewhere else or if we always
     #  automatically populate caltarget element
     "CALTARGET_ELEMENT",
     "LOCATION",
+    "SITE",
+    "DRIVE",
+    "RSM",
     "ZOOM",
     "L_S",
+    "SCLK",
     "SOLAR_ELEVATION",
-    "SOLAR_AZIMUTH",
+    "INCIDENCE_ANGLE",
+    "EMISSION_ANGLE",
     # TODO: RC-file value. ?
     "AZIMUTH_ANGLE",
+    "PHASE_ANGLE",
+    "LTST",
+    "LAT",
+    "LON",
+    "ODOMETRY",
+    "ROVER_ELEVATION",
+    "TARGET_ELEVATION",
     "CREATOR",
     "FILE_TIMESTAMP",
     "ROI_SOURCE",
     "ORIGINAL_ROI_SOURCE",
+    "INSTRUMENT",
     "COMPRESSION",
     "COMPRESSION_QUALITY",
+    *FILTER_DATA_COLUMNS,
     "ROW",
     "COLUMN",
     "DET_RAD",
     "DET_THETA",
-    *[f"RC_{col}" for col in RC_METADATA_COLUMNS],
-    "FORMAT_VERSION",
+    *[f"RC_{col}" for col in RC_METADATA_COLUMNS]
 )
 
 # statistical columns we add along with mean value to FILTER_DATA_COLUMNS
-COMPACT_MARSLAB_STATS = ["STD", "COUNT"]
+COMPACT_MARSLAB_STATS = ["ERR", "COUNT"]
 
 
 # mapping from field names to label parameters.
@@ -220,8 +218,20 @@ IOF_METADATA_FIELDS = {
         'keys': ('COMPRESSION_PARMS', 'INST_CMPRS_QUALITY')
     },
     "BAYER": "BAYER_METHOD",
+    "SOLAR_ELEVATION": "SOLAR_ELEVATION",
+    "SOLAR_AZIMUTH": "SOLAR_AZIMUTH",
     "SCLK": "SPACECRAFT_CLOCK_START_COUNT",
     "COMPLETION": "PRODUCT_COMPLETION_STATUS",
+    # TODO: check if they're in the headers now
+    # these files appear to currently be stored in
+    # # /project/m2020/gds/radcal/effective_taus on islamorada
+    "TAU_ESTIMATE_FILENAME": "TAU_ESTIMATE_FILENAME",
+    "INSTRUMENT_ELEVATION": {
+        "keys": ("SITE_DERIVED_GEOMETRY_PARMS", "INSTRUMENT_ELEVATION")
+    },
+    "INSTRUMENT_AZIMUTH": {
+        "keys": ("SITE_DERIVED_GEOMETRY_PARMS", "INSTRUMENT_AZIMUTH")
+    },
     "INPUT_PRODUCT_ID": "INPUT_PRODUCT_ID",
     "RC_FILE": "RC_FILE",
     # subframe parameters to be assembled later
@@ -230,6 +240,45 @@ IOF_METADATA_FIELDS = {
     "LINES": "LINES",
     "LINE_SAMPLES": "LINE_SAMPLES",
 }
+
+# # regexes for getting metadata from attached PDS3 product labels without
+# # parsing PVL. this structure defines almost everything we look for in a label.
+# IOF_METADATA_REGEX = {
+#     # the zoom motor count is given several places in the label,
+#     # but the malin mini header line has other interesting contents
+#     "MINI_HEADER": r"(?<=ARTICULATION_DEV_POSITION ).*(\(.*\))",
+#     "FRAME_TYPE": r"(?<=FRAME_TYPE ).*?(\w+)",
+#     "RMC": r"(?<=ROVER_MOTION_COUNTER ).*(\(.*\))",
+#     "SEQ_ID": r"(?<=SEQUENCE_ID).*(zcam\d+)",
+#     "SOL": r"(?<=PLANET_DAY_NUMBER).*?(\d+)",
+#     "FILTER": r"FILTER_NAME.*ZCAM_([LR][\w\d])(?=_)",
+#     "IMAGE_TIME": r"(?<=IMAGE_TIME ).*?([\d\-T:]+)",
+#     "LTST": r"(?<=LOCAL_TRUE_SOLAR_TIME ).*?([\d:]+)",
+#     "LMST": r"(?<=LOCAL_MEAN_SOLAR_TIME ).*?M([\d:]+)",
+#     "PRODUCT_CREATION_TIME": r"(?<=PRODUCT_CREATION_TIME ).*?([\d\-T:]+)",
+#     "L_S": r"(?<=SOLAR_LONGITUDE ).*?([\d\.]+)",
+#     "COMPRESSION": r"(?<=INST_CMPRS_NAME ).*?(\w+)",
+#     # note that JPEG compression is rendered as a negative number under
+#     # IMG_REQUEST_PARMS, which is why we're specifying the one from
+#     # COMPRESSION_PARMS here
+#     "COMPRESSION_QUALITY": r"(?:COMPRESSION_PARMS("
+#     r"?:\n|\r|.)*?INST_CMPRS_QUALITY ).*?([-\d]+)",
+#     "BAYER": r"(?<=BAYER_METHOD ).*?([\w_]+)",
+#     "SOLAR_ELEVATION": r"(?<=SOLAR_ELEVATION ).*?([\d\.]+)",
+#     "SOLAR_AZIMUTH": r"(?<=SOLAR_AZIMUTH ).*?([\d\.]+)",
+#     "SCLK": r"(?<=SPACECRAFT_CLOCK_START_COUNT ).*?([\d\.]+)",
+#     "COMPLETION": r"(?<=PRODUCT_COMPLETION_STATUS ).*?([\w_]+)",
+#     # TODO: check if they're in the headers now
+#     # these files appear to currently be stored in
+#     # # /project/m2020/gds/radcal/effective_taus on islamorada
+#     "TAU_ESTIMATE_FILENAME": r"(?<=TAU_ESTIMATE_FILENAME).*?(\w+\.csv)",
+#     "INSTRUMENT_ELEVATION": r"(?:SITE_DERIVED_GEOMETRY_PARMS("
+#     r"?:\n|\r|.)*?INSTRUMENT_ELEVATION ).*?(["
+#     r"-\d\.]+)",
+#     "INSTRUMENT_AZIMUTH": r"(?:SITE_DERIVED_GEOMETRY_PARMS("
+#     r"?:\n|\r|.)*?INSTRUMENT_AZIMUTH ).*?([-\d\.]+)",
+# }
+# quantities and take the value of their 'value' keys, ignoring 'units'.
 
 # Define the types of pixel flags that we care about
 PIXEL_FLAG_NAMES = ("bad", "no_signal", "nonlinear", "saturated", "hot")
--- asdf_settings/sources.py
+++ asdf_settings/sources.py
@@ -9,7 +9,8 @@ PUBLIC_WAYPOINTS_URL = (
 FIND_EFFECTIVE_TAUS = True
 EFFECTIVE_TAU_PATH = "/project/m2020/gds/radcal/effective_taus/"
 
-META_ROOTS = ["/scratch/cal_wg/flight/products/"]
+PIX_ROOTS = ["/scratch/cal_wg/flight/products/"]
+RC_ROOTS = ["/scratch/cal_wg/flight/products/"]
 
 # resolvers for asdf -a
 PATH_ABBREVIATIONS = {
@@ -34,11 +35,11 @@ AWS_REGION = "us-east-1"
 OBFUSCATE_THUMBNAIL_NAMES = True
 
 # debug-mode locations for testing uploads without dirtying live content
-# DEBUG_GOOGLE_SHEET_ID = "18r9wBG9rnVGQOv7XglqsZJmXK7ebBupLAJxHAIr_uH8"
-# DEBUG_METADATA_BACKUP_FOLDER_ID = "1pqP4Ohdsc8ACTh9EQ1HAvjFyL11fgiXE"
-# DEBUG_GOOGLE_DRIVE_ROOT = "1Fv7oKhwdz3pu4FCzvLVLUkcfx7VFX1Rx"
+DEBUG_GOOGLE_SHEET_ID = "18r9wBG9rnVGQOv7XglqsZJmXK7ebBupLAJxHAIr_uH8"
+DEBUG_METADATA_BACKUP_FOLDER_ID = "1pqP4Ohdsc8ACTh9EQ1HAvjFyL11fgiXE"
+DEBUG_GOOGLE_DRIVE_ROOT = "1Fv7oKhwdz3pu4FCzvLVLUkcfx7VFX1Rx"
 
 # 'staging' locations
-DEBUG_GOOGLE_SHEET_ID = "16Xznv0YodjvneaBN8-5mSNSbsR6NJIpWK2vFbW4ncEU"
-DEBUG_METADATA_BACKUP_FOLDER_ID = "182dVJ1WKMdTSBrn4mqFl-UZBOijT8qvC"
-DEBUG_GOOGLE_DRIVE_ROOT = "1bpgoxWdFcckJgMQg8ww96VcoopuJuJ1V"
+# DEBUG_GOOGLE_SHEET_ID = "13MdF17kllHHyEB1PNM-noAjNY598HWpBHtbrdhzE4l0"
+# DEBUG_METADATA_BACKUP_FOLDER_ID = "1iT1Wo_thD0ke0i4kSDNXM-bspOrcwLcU"
+# DEBUG_GOOGLE_DRIVE_ROOT = "1EridSo6YM6udKNjmlQ2WJr3S-ZBYo2gx"
