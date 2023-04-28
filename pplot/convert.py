4f29f3a8c4a87d0a1fa46549e5c6eff15fc34bc4
Sierra-MC <sierra@millionconcepts.com>
2023-04-28 15:57:40 -0400

    remove old merspect_to_marslab functionality

--- pplot/convert.py
+++ pplot/convert.py
@@ -4,7 +4,6 @@
 
 import pandas as pd
 import numpy as np
-from marslab.compat.xcam import WAVELENGTH_TO_FILTER
 
 
 def scale_eyes(data, method="scale_to_avg"):
@@ -60,151 +59,6 @@ def scale_eyes(data, method="scale_to_avg"):
     return data
 
 
-def merspect_to_marslab(
-    spectra_fn,  # a MERspect exported spectra file, unmodified
-    instrument="ZCAM",  # valid options are ZCAM, MCAM
-    color_to_feature={},  # a mapping between color names and feature names
-):
-    csv = pd.read_csv(spectra_fn)
-    if not "# Wavelength (nm)" in csv.keys():
-        raise IOError("This seems to not be a MERspect formatted file.")
-    csv.rename(columns={"# Wavelength (nm)": "Wavelength"}, inplace=True)
-    [csv.rename(columns={k: k.strip()}, inplace=True) for k in csv.keys()]
-    # We want the columns in order of ascending wavelength, regardless of instrument
-    # Columns that don't have corresponding data will just be given values of NaN
-    columns = {  # Some of these parameters are only meaningful to WWU Marslab
-        "MCAM": [
-            "SOL",
-            "SEQ_ID",
-            "INSTRUMENT",
-            "COLOR",
-            "FEATURE",
-            "FORMATION",
-            "MEMBER",
-            "FLOAT",
-            "L2",
-            "L2_ERR",
-            "R2",
-            "R2_ERR",
-            "L0B",
-            "L0B_ERR",
-            "R0B",
-            "R0B_ERR",
-            "L1",
-            "L1_ERR",
-            "R1",
-            "R1_ERR",
-            "R0G",
-            "R0G_ERR",
-            "L0G",
-            "L0G_ERR",
-            "R0R",
-            "R0R_ERR",
-            "L0R",
-            "L0R_ERR",
-            "L4",
-            "L4_ERR",
-            "L3",
-            "L3_ERR",
-            "R3",
-            "R3_ERR",
-            "L5",
-            "L5_ERR",
-            "R4",
-            "R4_ERR",
-            "R5",
-            "R5_ERR",
-            "L6",
-            "L6_ERR",
-            "R6",
-            "R6_ERR",
-        ],  # Some of these parameters are only meaningful to WWU Marslab
-        "ZCAM": [
-            "SOL",
-            "SEQ_ID",
-            "INSTRUMENT",
-            "COLOR",
-            "FEATURE",
-            "FORMATION",
-            "MEMBER",
-            "FLOAT",
-            "L6",
-            "L6_ERR",
-            "L0B",
-            "L0B_ERR",
-            "R0B",
-            "R0B_ERR",
-            "L5",
-            "L5_ERR",
-            "L0G",
-            "L0G_ERR",
-            "R0G",
-            "R0G_ERR",
-            "L4",
-            "L4_ERR",
-            "L0R",
-            "L0R_ERR",
-            "R0R",
-            "R0R_ERR",
-            "L3",
-            "L3_ERR",
-            "L2",
-            "L2_ERR",
-            "L1",
-            "L1_ERR",
-            "R1",
-            "R1_ERR",
-            "R2",
-            "R2_ERR",
-            "R3",
-            "R3_ERR",
-            "R4",
-            "R4_ERR",
-            "R5",
-            "R5_ERR",
-            "R6",
-            "R6_ERR",
-        ],
-    }[instrument]
-    data = pd.DataFrame(columns=columns)
-
-    # Generate the index column --- 'COLOR'
-    colors = [
-        " ".join(k.split(" ")[:-2])
-        for k in csv.keys()
-        if k.endswith("Mean Value")
-    ]
-    data["COLOR"] = colors
-    data = data.set_index("COLOR")
-
-    for color in colors:
-        this_color = csv[
-            [
-                "Eye",
-                "Wavelength",
-                f"{color} Mean Value",
-                f"{color} Standard Deviation",
-            ]
-        ]
-        for i in range(len(this_color)):
-            eye = this_color.loc[i]["Eye"].strip()[0]
-            wavelength = int(this_color.loc[i]["Wavelength"])
-            filt = WAVELENGTH_TO_FILTER["ZCAM"][eye][wavelength]
-            data[f"{filt}"].loc[color] = this_color.loc[i][
-                f"{color} Mean Value"
-            ]
-            data[f"{filt}_STD"].loc[color] = this_color.loc[i][
-                f"{color} Standard Deviation"
-            ]
-        # Add in the feature names, if given
-        if color in color_to_feature.keys():
-            data["FEATURE"].loc[color] = color_to_feature[color]
-
-    data.reset_index(inplace=True)
-    data = data[columns]
-    return data
-
-
 def convert_for_plot(
     spectra_fn,
     instrument="ZCAM",
@@ -215,15 +69,7 @@ def convert_for_plot(
         spectra_fn = [spectra_fn]
     data = pd.DataFrame()
     for fn in spectra_fn:
-        try:
-            # First try to convert MERspect to Marslab format
-            marslab_data = merspect_to_marslab(
-                fn,
-                instrument=instrument,
-                color_to_feature=color_to_feature,
-            )
-        except IOError:
-            marslab_data = pd.read_csv(fn, index_col=None, na_values="-")
+        marslab_data = pd.read_csv(fn, index_col=None, na_values="-")
         data = pd.concat([data, marslab_data], ignore_index=True)
     data = scale_eyes(data, method=scale_method)
     data.replace(np.nan, "-", inplace=True)
