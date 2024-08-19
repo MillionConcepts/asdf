cf916af48e74106c54e6d1f85b8862359926843f
m-stclair <mstclair@millionconcepts.com>
2024-08-19 14:02:35 -0400

    tweak argv for Fire compatibility while retaining CLI idiom

--- asdf.py
+++ asdf.py
@@ -3,7 +3,23 @@
 import fire
 
 
+# TODO: just replace Fire entirely with argparse
+def rearrange_args(args):
+    """
+    Rearrange args for Fire, permitting flags both before and after
+    positional arguments.
+    """
+    base, pos, flag = [args[0]], [], []
+    for a in args[1:]:
+        target = flag if a.startswith('-') else pos
+        target.append(a)
+    return base + pos + flag
+
+
 # tell fire to handle command line call
 if __name__ == "__main__":
     import asdf.cli_endpoint
+    import sys
+
+    sys.argv = rearrange_args(sys.argv)
     fire.Fire(asdf.cli_endpoint.asdf_initiate)
