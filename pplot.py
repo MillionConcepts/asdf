2d4da1b7bd2ec5734494dcd673a0388352ac196a
m-stclair <mstclair@millionconcepts.com>
2025-01-24 16:03:26 -0500

    update pretty-plot hook

--- pplot.py
+++ pplot.py
@@ -1,12 +1,18 @@
 """user-facing noninteractive pretty_plot utility"""
+import sys
 
-import fire
-
-# import importlib
-# pretty_plot = importlib.import_module("pretty-plot")
-
-import pretty_plot
 
 # tell fire to handle command line call
 if __name__ == '__main__':
-    fire.Fire(pretty_plot.cli.do_pplot)
+    try:
+        import fire
+    except ImportError:
+        print(
+            "'fire' package not found. Did you "
+            "forget to activate a virtual environment?"
+        )
+        sys.exit(1)
+
+    from pretty_plot.cli import do_pplot
+
+    fire.Fire(do_pplot)
